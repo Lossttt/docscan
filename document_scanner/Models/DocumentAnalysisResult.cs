6d31bde52379e936c1a5d6538ad6eaed388c19a4
Lossttt <lfatmi.ayoub@gmail.com>
2024-06-10 15:07:04 +0200

    Updated DocumentAnalysisResult model

--- document_scanner/Models/DocumentAnalysisResult.cs
+++ document_scanner/Models/DocumentAnalysisResult.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace document_scanner.Models
 {
     public class DocumentAnalysisResult
     {
+        [Required]
         public bool Success { get; set; }
         public string? Message { get; set; }
         public string? Quality { get; set; }
