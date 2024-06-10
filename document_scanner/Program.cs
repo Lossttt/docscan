90e8374fffbbd4c8b17d0ac3574ea19f8a9a78a7
Lossttt <lfatmi.ayoub@gmail.com>
2024-06-10 15:03:59 +0200

    Added scoped service containers, db service connection

--- document_scanner/Program.cs
+++ document_scanner/Program.cs
@@ -1,7 +1,13 @@
+using document_scanner.Data;
 using document_scanner.Middleware;
 using document_scanner.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add database context to the container
+// The AddDbContext method is used to register the FileDbContext class with the dependency injection container.
+builder.Services.AddDbContext<FileDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -10,6 +16,8 @@ builder.Services.AddAntiforgery(options =>
     options.HeaderName = "X-CSRF-TOKEN";
 });
 
+// Add the HttpContextAccessor and CORS services to the container
+// The AddHttpContextAccessor method is used to register the HttpContextAccessor class with the dependency injection container.
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddCors(options =>
 {
@@ -19,7 +27,9 @@ builder.Services.AddCors(options =>
                           .AllowAnyMethod());
 });
 
+// Register the services with the dependency injection container
 builder.Services.AddScoped<IDocumentService, DocumentService>();
+builder.Services.AddScoped<IUploadService, UploadService>();
 
 var app = builder.Build();
 
@@ -30,6 +40,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 // Add the antiforgery tokens middleware here
+// The UseAntiforgeryTokens method is used to add the AntiforgeryTokensMiddleware to the HTTP request pipeline.
 app.UseAntiforgeryTokens();
 
 app.UseHttpsRedirection();
